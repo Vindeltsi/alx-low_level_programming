4488fcc8bfa8ad9d43fcc1493969f4a37c31c217
Vindeltsi <tsikvincentia@gmail.com>
2023-06-17 18:38:36 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int lowerCase = 'a';
+int upperCase = 'A';
+while (lowerCase <= 'z')
+{
+putchar(lowerCase);
+lowerCase += 1;
+}
+while (upperCase <= 'Z')
+{
+putchar(upperCase);
+}
+putchar('\n');
+return (0);
+}
